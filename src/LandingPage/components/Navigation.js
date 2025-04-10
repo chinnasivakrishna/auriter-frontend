9bb37c3db2b10a945d6b6647cc06a902c708c5dd
chinnasivakrishna <chinnasivakrishna2003@gmail.com>
2025-04-10 18:27:53 +0530

    sadf

--- src/LandingPage/components/Navigation.js
+++ src/LandingPage/components/Navigation.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
-import logo from "./images/Airuter Logo_White Background.jpg";
+import logo from "./images/Airuter LOGO_Without Background.png";
 // import { Link, useLocation } from "react-router-dom";
 export default function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
